43b19539fc758e55d47fc4744a337b106d68ac99
Renzo Vinci <renzvinci@gmail.com>
2020-11-27 12:51:48 -0300

    s

--- resources/views/enviados/modal.blade.php
+++ resources/views/enviados/modal.blade.php
@@ -1,8 +1,8 @@
 <div class="modal fade" id="modalImportar"  role="dialog" aria-labelledby="modalImportRegistroLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog  modal-lg" role="document">
       <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Importar Entregas</h5>
+        <div class="modal-header" style="background-color: #3e5569; color: #fff">
+          <h5 class="modal-title" id="exampleModalLabel" style="background-color: #3e5569; color: #fff">Importar Entregas</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
             <span aria-hidden="true">&times;</span>
           </button>
@@ -14,7 +14,8 @@
                         <div class="form-group row">
                           <div class="col-md-5">
                               <div>
-                                  <label for="alias" class="control-label col-form-label">Adjunte el archivo en formato Excel. Recuerde que la primer fila no será leída y contada como el titulo de la columna</label>
+                                  <label for="alias" class="control-label col-form-label">Adjunte el archivo en formato Excel. </label>
+                                  <small id="emailHelp" class="form-text text-muted">Recuerde que la primer fila no será leída y contada como el titulo de la columna</small>
                               </div>
                           </div>
                           <div class="col-md-7">
