acfc458a460bccd2132e7d64186d5af86731d084
Renzo Vinci <renzvinci@gmail.com>
2020-11-27 13:08:00 -0300

    s

--- app/Imports/EnviadoImport.php
+++ app/Imports/EnviadoImport.php
@@ -8,18 +8,15 @@ use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\WithStartRow;
-use Maatwebsite\Excel\Concerns\RemembersRowNumber;
 
 
 class EnviadoImport implements ToModel, WithBatchInserts, WithChunkReading,WithStartRow
 {
 
-    use RemembersRowNumber;
 
     public function model(array $row)
     {
-        $currentRowNumber = $this->getRowNumber();
-        
+
         $existe = Enviado::where('documento','=', $row[0])->first();
         if(empty($existe)){
             $nuevo = (new Enviado);
