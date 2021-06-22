c9c325b6c90756ef01a81e8213fdc1d05c2bc6ec
Renzo Vinci <renzvinci@gmail.com>
2021-06-22 12:29:59 -0300

    cambios

--- resources/views/sanrafael.blade.php
+++ resources/views/sanrafael.blade.php
@@ -5,18 +5,18 @@
 	<meta charset="utf-8">
 	<title>SUBE San Rafael</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="sitio/assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/sitio/assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.css">
-	<link rel="stylesheet" href="sitio/assets/css/animate.min.css">
-	<link rel="stylesheet" href="sitio/assets/css/fontawesome-all.css">
-	<link rel="stylesheet" href="sitio/assets/css/style.css">
+	<link rel="stylesheet" href="/sitio/assets/css/animate.min.css">
+	<link rel="stylesheet" href="/sitio/assets/css/fontawesome-all.css">
+	<link rel="stylesheet" href="/sitio/assets/css/style.css">
 
 	<link rel="stylesheet" type="text/css" href="sitio/assets/css/colors/switch.css">
 	<!-- Color Alternatives -->
-	<link href="sitio/assets/css/colors/color-2.css" rel="alternate stylesheet" type="text/css" title="color-2">
-	<link href="sitio/assets/css/colors/color-3.css" rel="alternate stylesheet" type="text/css" title="color-3">
-	<link href="sitio/assets/css/colors/color-4.css" rel="alternate stylesheet" type="text/css" title="color-4">
-	<link href="sitio/assets/css/colors/color-5.css" rel="alternate stylesheet" type="text/css" title="color-5">
+	<link href="/sitio/assets/css/colors/color-2.css" rel="alternate stylesheet" type="text/css" title="color-2">
+	<link href="/sitio/assets/css/colors/color-3.css" rel="alternate stylesheet" type="text/css" title="color-3">
+	<link href="/sitio/assets/css/colors/color-4.css" rel="alternate stylesheet" type="text/css" title="color-4">
+	<link href="/sitio/assets/css/colors/color-5.css" rel="alternate stylesheet" type="text/css" title="color-5">
 
 	<link rel="stylesheet" href="/css/jquery-confirm.min.css">
 	<style>
@@ -195,14 +195,14 @@
 			</div>
 		</form>
 	</div>
-	<script src="sitio/assets/js/jquery-3.3.1.min.js"></script>
-	<script src="sitio/assets/js/jquery.validate.min.js"></script>
-	<script src="sitio/assets/js/input.js"></script>
+	<script src="/sitio/assets/js/jquery-3.3.1.min.js"></script>
+	<script src="/sitio/assets/js/jquery.validate.min.js"></script>
+	<script src="/sitio/assets/js/input.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
-	<script src="sitio/assets/js/bootstrap.min.js"></script>
-	<script src="js/jquery-confirm.min.js"></script>
-	<script src="sitio/assets/js/main.js?version=20"></script>
-    <script src="sitio/assets/js/switch.js"></script>
+	<script src="/sitio/assets/js/bootstrap.min.js"></script>
+	<script src="/js/jquery-confirm.min.js"></script>
+	<script src="/sitio/assets/js/main.js?version=20"></script>
+    <script src="/sitio/assets/js/switch.js"></script>
 	
 	
 	<script>
