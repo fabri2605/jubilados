9b310b49ac533f4ba8c601a1e3906b9e80b98528
Renzo Vinci <renzvinci@gmail.com>
2021-06-27 13:32:39 -0300

    version 2

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 Route::get('/', 'SitioController@landing')->name('inicio');
+Route::get('/testing', 'SitioController@landing')->name('testing');
 Route::get('/sanrafael', 'SitioController@landingSR')->name('inicio_sanrafael');
 
 Route::get('/particulares', 'SitioController@particulares')->name('particulares');
