e8d2a402a5f27a264446dff0858cc76e995e6138
Renzo Vinci <renzvinci@gmail.com>
2021-06-29 13:35:14 -0300

    cambios

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/particulares', 'SitioController@particulares')->name('particulares'
 Route::get('/sanrafael/solicitud', 'SitioController@sanrafael')->name('sanrafael');
 Route::get('/inicio', 'SitioController@sitio')->name('home');
 Route::get('/testing/sanrafael', 'SitioController@sitioSanRafael')->name('home_san_rafael');
-Route::get('/sanrafael', 'SitioController@landing')->name('home_san_rafael');
+Route::get('/sanrafael', 'SitioController@landing')->name('home_sr');
 
 Route::get('/retira', 'SitioController@retirala')->name('retirala');
 Route::get('/home', 'HomeController@index')->name('menu');
